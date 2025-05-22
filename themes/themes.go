e0c46f6ae3ff7fe563e348a28f6597d58f37982e
Sebastian Döll <sebastian@katallaxie.dev>
2025-05-22 05:37:00 +0000

    feat: add theme name list

---
+++ themes/themes.go
@@ -0,0 +1,42 @@
+package themes
+
+// Theme is a type that represents a theme.
+type Theme string
+
+// Theme constants
+const (
+	Abyss        Theme = "abyss"
+	Acid         Theme = "acid"
+	Aqua         Theme = "aqua"
+	Autumn       Theme = "autumn"
+	Black        Theme = "black"
+	Bumblebee    Theme = "bumblebee"
+	Business     Theme = "business"
+	Caramellatte Theme = "caramelatte"
+	Coffee       Theme = "coffee"
+	Corporate    Theme = "corporate"
+	Cupcake      Theme = "cupcake"
+	CYMK         Theme = "cmyk"
+	Dark         Theme = "dark"
+	Dim          Theme = "dim"
+	Dracula      Theme = "dracula"
+	Emerald      Theme = "emerald"
+	Fantasy      Theme = "fantasy"
+	Forest       Theme = "forest"
+	Garden       Theme = "garden"
+	Halloween    Theme = "halloween"
+	Lemonade     Theme = "lemonade"
+	Light        Theme = "light"
+	Lofi         Theme = "lofi"
+	Luxury       Theme = "luxury"
+	Night        Theme = "night"
+	Nord         Theme = "nord"
+	Pastel       Theme = "pastel"
+	Retro        Theme = "retro"
+	Silk         Theme = "silk"
+	Sunset       Theme = "sunset"
+	Synthwave    Theme = "synthwave"
+	Valentine    Theme = "valentine"
+	Winter       Theme = "winter"
+	Wireframe    Theme = "wireframe"
+)
